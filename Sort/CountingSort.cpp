e838ffd1dbfeb4c2bec456602af56cf90c05306d
Othern <willy910928@gmail.com>
2025-01-06 17:18:08 +0800

    counting sort adjustment and delete searchAndSort file

--- Sort/CountingSort.cpp
+++ Sort/CountingSort.cpp
@@ -8,18 +8,19 @@ vector<int> CountingSort(vector<int> data){
     for(int i=0; i<=9; i++)count.push_back(0);
     for(int i=0; i< data.size(); i++)count[data[i]]++;
 
-    vector<int> end;
-    for(int i=0; i<=9; i++)end.push_back(0);
-    end[0] = count[0] -1;
+    // 由start 開始放，才會stable
+    vector<int> start;
+    for(int i=0; i<=9; i++)start.push_back(0);
+    start[0] = 0;
     for(int i=1; i<=9; i++){
-        end[i] = end[i-1] + count[i];
+        start[i] = count[i-1] + start[i-1];
     }
     vector<int> result;
     for(int i=0; i< data.size(); i++)result.push_back(0);
 
     for(int i=0; i< data.size(); i++){
-        result[end[data[i]]] = data[i];
-        end[data[i]]--;
+        result[start[data[i]]] = data[i];
+        start[data[i]]++;
     }
     return result;
 }
